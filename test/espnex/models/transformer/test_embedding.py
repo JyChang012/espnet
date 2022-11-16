9a04ca28d380081c9a4692711887995c50d910ac
Jiayu Chang <jychang012@outlook.com>
2022-11-16 17:20:04 -0500

    Fix CI

--- test/espnex/models/transformer/test_embedding.py
+++ test/espnex/models/transformer/test_embedding.py
@@ -1,34 +1,35 @@
 import jax
 import jax.numpy as jnp
-from pytest import mark
-from jax.random import PRNGKey
 import torch
+from jax.random import PRNGKey
 from numpy.testing import assert_allclose
+from pytest import mark
 
-from espnex.models.transformer.embedding import AddPositionalEncoding
 from espnet.nets.pytorch_backend.transformer.embedding import PositionalEncoding
+from espnex.models.transformer.embedding import AddPositionalEncoding
 
 rtol = 1e-6
 atol = 1e-6
 
 
-@mark.parametrize('b', [1, 5])
-@mark.parametrize('t', [2, 8])
-@mark.parametrize('d', [2, 6])
-@mark.parametrize('lens', [[10, 20]])
-@mark.parametrize('deterministic', [True, False])
-@mark.parametrize('jit', [True, False])
-@mark.parametrize('init_type', ['espnet', 'default'])
+@mark.parametrize("b", [1, 5])
+@mark.parametrize("t", [2, 8])
+@mark.parametrize("d", [2, 6])
+@mark.parametrize("lens", [[10, 20]])
+@mark.parametrize("deterministic", [True, False])
+@mark.parametrize("jit", [True, False])
+@mark.parametrize("init_type", ["espnet", "default"])
 def test_embedding(b, t, d, lens, deterministic, jit, init_type):
     le, ext_le = lens
     t = min(le, t)
     x = jnp.zeros([b, t, d], dtype=float)
-    model = AddPositionalEncoding(dropout_rate=.5, max_len=le, init_type=init_type)
+    model = AddPositionalEncoding(dropout_rate=0.5, max_len=le, init_type=init_type)
 
-    rngs = {'dropout': PRNGKey(0)}
+    rngs = {"dropout": PRNGKey(0)}
 
     def apply(x, rngs):
         return model.apply({}, x, deterministic, rngs=rngs)
+
     if jit:
         apply = jax.jit(apply)
 
@@ -37,10 +38,9 @@ def test_embedding(b, t, d, lens, deterministic, jit, init_type):
     # test auto extend embeddings length, supported by the torch version
     ext_x = jnp.empty([b, ext_le, d])
     ext_y = apply(ext_x, rngs)
-    if deterministic and init_type == 'espnet':
-        torch_encoder = PositionalEncoding(d, .5, le)
+    if deterministic and init_type == "espnet":
+        torch_encoder = PositionalEncoding(d, 0.5, le)
         torch_encoder.eval()
         x = torch.zeros([b, t, d])
         ty = torch_encoder(x)
         assert_allclose(jax.device_get(y), ty.numpy(), rtol=rtol, atol=atol)
-
--- test/espnex/models/transformer/test_subsampling.py
+++ test/espnex/models/transformer/test_subsampling.py
@@ -1,25 +1,27 @@
 import jax
-from pytest import mark
-from jax.random import PRNGKey, split
 import jax.numpy as jnp
+from jax.random import PRNGKey, split
+from pytest import mark
 
-from espnex.models.transformer.subsampling import defaults_settings, get_default_conv2d_subsampling
-
+from espnex.models.transformer.subsampling import (
+    defaults_settings,
+    get_default_conv2d_subsampling,
+)
 
 key = PRNGKey(15213)
 rngs = split(key, 2)
-rngs = dict(zip(['params', 'dropout'], rngs))
+rngs = dict(zip(["params", "dropout"], rngs))
 
 
-@mark.parametrize('in_shape', [[1, 256, 128]])
-@mark.parametrize('ratio', defaults_settings)
-@mark.parametrize('odim', [1, 5])
-@mark.parametrize('deterministic', [True, False])
-@mark.parametrize('jit', [True, False])
+@mark.parametrize("in_shape", [[1, 256, 128]])
+@mark.parametrize("ratio", defaults_settings)
+@mark.parametrize("odim", [1, 5])
+@mark.parametrize("deterministic", [True, False])
+@mark.parametrize("jit", [True, False])
 def test_subsampling(in_shape, ratio, odim, deterministic, jit):
-    model = get_default_conv2d_subsampling(odim, ratio, .5)
+    model = get_default_conv2d_subsampling(odim, ratio, 0.5)
     x = jnp.zeros(in_shape)
-    mask = jax.random.bernoulli(key, .5, in_shape[:-1])
+    mask = jax.random.bernoulli(key, 0.5, in_shape[:-1])
     variables = model.init(rngs, x, mask, deterministic)
 
     def apply(v, x, mask, rngs):
@@ -30,4 +32,3 @@ def test_subsampling(in_shape, ratio, odim, deterministic, jit):
     y, y_mask = apply(variables, x, mask, rngs)
     y = apply(variables, x, None, rngs)
     # TODO: add equality test
-
