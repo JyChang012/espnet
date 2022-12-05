fdfa95a9820adbc02aaaa6394c8372dd2dea63c6
Jiayu Chang <jychang012@outlook.com>
2022-12-04 20:39:53 -0500

    Modify implementation of make_pad_mask Now support generating mask of multidimensional tensors. Need more docs

--- espnex/models/utils.py
+++ espnex/models/utils.py
@@ -5,8 +5,9 @@ from jax import Array
 
 
 def make_pad_mask(  # a simplified version of the torch one, to support JIT compilation.
-    lengths: Array,
-    maxlen: int,
+        lengths: Array,
+        maxlen: int,
+        axis=-1
 ) -> Array:
     """Make mask tensor containing indices of padded part.
 
@@ -18,9 +19,15 @@ def make_pad_mask(  # a simplified version of the torch one, to support JIT comp
     Returns:
         Tensor: Boolean mask array containing indices of padded part.
     """
-    bs = lengths.shape
-    mask = jnp.arange(maxlen)
-    mask = jnp.broadcast_to(mask, [*bs, maxlen])  # (*bs, max_len)
-    lengths = jnp.expand_dims(lengths, -1)
-    mask = mask >= lengths  # (*bs, max_len)
+    in_shape = lengths.shape
+    axis = len(in_shape) + 1 + axis if axis < 0 else axis
+    prefix_shape = in_shape[:axis]
+    suffix_shape = in_shape[axis:]
+    lengths = jnp.expand_dims(lengths, axis=axis)  # (..., 1, ...)
+    out_shape = prefix_shape + (maxlen,) + suffix_shape
+    lengths = jnp.broadcast_to(lengths, out_shape)  # (..., maxlen, ...)
+    mask = jnp.arange(maxlen)  # [0, 1, 2, ..., maxlen], (maxlen,)
+    mask = jnp.expand_dims(mask, [1 + i for i in range(len(suffix_shape))])
+    # (1, 1, ..., 1, maxlen, 1, ..., 1)
+    mask = mask >= lengths
     return mask
