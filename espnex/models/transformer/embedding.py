2d387f4a5940309080ba3f783c8bdf48e133f6aa
Jiayu Chang <jychang012@outlook.com>
2022-12-29 23:49:32 -0500

    Fix a bug in embedding.py.

--- espnex/models/transformer/embedding.py
+++ espnex/models/transformer/embedding.py
@@ -1,5 +1,5 @@
 import math
-from typing import Literal, Optional
+from typing import Literal, Optional, Sequence, no_type_check
 
 import flax.linen as nn
 import jax
@@ -11,6 +11,7 @@ from jax import Array
 from jax.random import bernoulli
 
 
+@no_type_check
 def sinusoidal_init(
     max_len=2048, min_scale=1.0, max_scale=10000.0, init_type="default"
 ):
@@ -23,9 +24,9 @@ def sinusoidal_init(
         output: init function returning `(1, max_len, d_feature)`
     """
 
-    def init(key, shape, dtype=np.float32):
+    def init(shape: Sequence[int], dtype=np.float32) -> Array:
         """Sinusoidal init."""
-        del key, dtype
+        del dtype
         d_feature = shape[-1]
         pe = np.zeros((max_len, d_feature), dtype=np.float32)
         position = np.arange(0, max_len)[:, np.newaxis]
@@ -36,16 +37,16 @@ def sinusoidal_init(
         pe = pe[np.newaxis, :, :]  # [1, max_len, d_feature]
         return jnp.array(pe)
 
-    def init_espnet(key, shape, dtype=np.float32):
+    def init_espnet(shape: Sequence[int], dtype=np.float32) -> Array:
         """Sinusoidal init."""
-        del key, dtype
+        del dtype
         d_feature = shape[-1]
-        pe = np.zeros((max_len, d_feature), dtype=np.float32)
-        position = np.arange(0, max_len)[:, np.newaxis]
+        pe = np.zeros((max_len, d_feature), dtype=np.float32)  # (max_len, d_feature)
+        position = np.arange(0, max_len)[:, np.newaxis]  # (max_len, 1)
         scale_factor = -np.log(max_scale / min_scale) / d_feature
-        div_term = min_scale * np.exp(np.arange(0, d_feature, 2) * scale_factor)
-        pe[:, ::2] = np.sin(position * div_term)
-        if div_term.shape[0] % 2:
+        div_term = min_scale * np.exp(np.arange(0, d_feature, 2) * scale_factor)  # (d_feature // 2,)
+        pe[:, ::2] = np.sin(position * div_term)  # (max_len, d_feature // 2)
+        if d_feature % 2:
             div_term = div_term[:-1]
         pe[:, 1::2] = np.cos(position * div_term)
         pe = pe[np.newaxis, :, :]  # [1, max_len, d_feature]
@@ -78,9 +79,7 @@ class AddPositionalEncoding(Module):
         length = x.shape[1]
         max_len = max(self.max_len, length)
         pos_emb_shape = (1, max_len, x.shape[-1])
-        pos_emb = sinusoidal_init(max_len=max_len, init_type=self.init_type)(
-            None, pos_emb_shape, None
-        )
+        pos_emb = sinusoidal_init(max_len=max_len, init_type=self.init_type)(pos_emb_shape)
         x_scale = np.sqrt(x.shape[-1])
         x = x * x_scale + pos_emb[:, :length, :]
         x = Dropout(self.dropout_rate)(x, deterministic)
